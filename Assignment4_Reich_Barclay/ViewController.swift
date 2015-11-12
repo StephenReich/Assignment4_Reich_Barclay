5d76fcf5b113348a233921ff79b8c01f636e1070
stephenreich <sjreich2@bsu.edu>
2015-11-12 11:04:50 -0500

    Initial Commit

---
+++ Assignment4_Reich_Barclay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment4_Reich_Barclay
+//
+//  Created by Stephen Reich on 11/12/15.
+//  Copyright © 2015 Stephen Reich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
